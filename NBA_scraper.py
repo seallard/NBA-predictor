0fb3d76bebd726440cf6834ce351eca3cd747536
seallard <seallard95@gmail.com>
2018-05-29 01:54:54 +0200

    Added collection of dates
    The date of each game is now written to the csv file as well.

--- NBA_scraper.py
+++ NBA_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 
 def soupify(url):
-    "Takes a url and returns document."
+    "Takes url and returns document."
     r = requests.get(url)
     soup = BeautifulSoup(r.text,"html5lib") 
     return soup
@@ -16,24 +16,39 @@ for ul_tag in soup.find_all('ul', {'class': 'medium-logos'}):
     for li_tag in ul_tag.find_all('li'):
         teams.append(li_tag.a.get('href').split('/')[7]) #Extract abbreviations.
 
-#Collect all game id:s for each team. 
+#Collect all game id:s and dates for each team. 
 game_ids = []
+dates = []
+months = ['Oct','Nov','Dec']
 
 for name in teams:
     soup = soupify('http://www.espn.com/nba/team/schedule/_/name/' + name + '/seasontype/2')
 
+    #Collecting dates.
+    data = soup.find_all('div',{'class':'mod-content'})
+
+    for date in data[0].find_all('td'):
+        if ',' in date.text: #Only date td contain ','.
+            if any(m in date.text for m in months):
+                year = ' 2017'
+            else:
+                year = ' 2018'
+            dates.append(year + date.text.split(',')[-1])
+
+    #Collecting id:s.
     for ul_tag in soup.find_all('ul', {'class': 'game-schedule'}):
         for li_tag in ul_tag.find_all('li',{'class': 'score'}):
             game = li_tag.a.get('href')[30:39] #Extract id number.
+
             if game not in game_ids: #Avoid duplicates.
                 game_ids.append(game)
 
 #Collect box scores for each collected game id and write to csv.
 with open('NBA_game_stats.csv','w') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['fg','3pt','ft','oreb','dreb','reb','ast','stl','blk','to','pf','pts','id','team'])
+    filewriter.writerow(['fg','3pt','ft','oreb','dreb','reb','ast','stl','blk','to','pf','pts','id','team','date'])
     
-    for game in game_ids:
+    for i,game in enumerate(game_ids):
         soup = soupify('http://www.espn.com/nba/boxscore?gameId=' + game)
         data = soup.find_all('tr',{'class': 'highlight'})
 
@@ -47,4 +62,5 @@ with open('NBA_game_stats.csv','w') as csvfile:
                     team_data.append(td_tag.text)
         
             team_data.append(game)
+            team_data.append(dates[i])
             filewriter.writerow(team_data[1:])
