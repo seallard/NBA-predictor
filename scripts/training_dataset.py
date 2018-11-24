9b71f1ed4cbc5523c6fe4ab7a1c085bfd6412cf5
seallard <seallard95@gmail.com>
2018-11-24 20:15:40 +0100

    Added team names to training data set.

--- scripts/training_dataset.py
+++ scripts/training_dataset.py
@@ -13,30 +13,35 @@ with open("clean_dataset.csv") as infile:
 with open("training_dataset.csv", "w") as outfile:
 
     filewriter = csv.writer(outfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        
+    
     # Write header. 
-    filewriter.writerow(['fg_h','3pt_h','ft_h','oreb_h','dreb_h','reb_h','ast_h','stl_h','blk_h',
-                        'to_h','pf_h','pts_h', 'fg_a','3pt_a','ft_a','oreb_a','dreb_a','reb_a',
-                        'ast_a','stl_a','blk_a', 'to_a','pf_a','pts_a', 'outcome', 'date'])
+    filewriter.writerow(['home_team', 'away_team', 'fg_h','3pt_h','ft_h','oreb_h','dreb_h','reb_h',
+                        'ast_h','stl_h','blk_h', 'to_h','pf_h','pts_h', 'fg_a','3pt_a','ft_a','oreb_a',
+                        'dreb_a','reb_a', 'ast_a','stl_a','blk_a', 'to_a','pf_a','pts_a', 'outcome', 'date'])
     
     # Creating training dataset csv file. 
     for i, row in enumerate(data[1:]):
 
-        # Two consecutive rows contains, in order, home and away team scores.
+        # Even rows contain away team score, odd contain home team score.  
         if i % 2:
-            away_team = data[i]
-            home_team = data[i+1][:-1]
+            away_stats = data[i][1:-1]
+            away_team = [data[i][0]]
             
-            # Check outcome of game.
-            if int(home_team[-1]) - int(away_team[-2]) > 0:
+            home_stats = data[i+1][:-1]
+            home_team = [data[i+1][0]]
 
-                outcome = 1 # Home team won. 
+            date = [row[-1]]
+
+            # Check outcome of game.
+            if int(home_stats[-1]) - int(away_stats[-1]) > 0:
+
+                outcome = [1] # Home team won. 
             
             else:
-                outcome = 0 # Home team lost.
+                outcome = [0] # Home team lost.
             
             # Save home, away statistics, date and outcome to one row.
-            filewriter.writerow(home_team + away_team[:-1] + [outcome] + [away_team[-1]])
+            filewriter.writerow(home_team + away_team + home_stats + away_stats + outcome + date)
               
 # Sort training data by date. 
 df = pd.read_csv("training_dataset.csv")
