47be9ff4f5ae00294a99902b7c15b8e56380b3fc
seallard <seallard95@gmail.com>
2018-12-02 01:48:37 +0100

    Added percentages to dataset.

--- scripts/dataset_cleaner.py
+++ scripts/dataset_cleaner.py
@@ -1,19 +1,41 @@
 import csv
 
-with open('raw_dataset.csv','r') as infile:
-    
-    outfile = open('clean_dataset.csv','w')
-    
-    filewriter = csv.writer(outfile)
+with open("raw_dataset.csv",'r') as infile:
+
+    next(infile) # Skip header.
+
+    outfile = open("clean_dataset.csv",'w', newline='')
+    writer = csv.writer(outfile)
+    writer.writerow(['team', 'pts', 'fg%', '3pt%', 'ft%', 'oreb', 'dreb', 'ast', 'stl', 'blk', 'to', 'date'])
 
     for row in infile:
         row = row.split(',')
 
-        # Replace xx-yy score with xx.
-        clean_row = [row[0].split('-')[0], row[1].split('-')[0], row[2].split('-')[0]] + row[3:12] 
+        # Calculate field goals made percentage. 
+        fg_made = row[0].split('-')[0]
+        fg_attempted = row[0].split('-')[-1]
+        fg_percent = round(int(fg_made)/int(fg_attempted), 4)
 
-        # Add value to date column.
-        clean_row.append(row[15].strip('\n'))
+        # Calculate three-points made percentage.
+        threept_made = row[1].split('-')[0]
+        threept_attempted = row[1].split('-')[-1]
+        threept_percent = round(int(threept_made)/int(threept_attempted), 4)
+
+        # Calculate freethrows made percentage. 
+        ft_made = row[2].split('-')[0]
+        ft_attempted = row[1].split('-')[-1]
+        ft_percent = round(int(ft_made)/int(ft_attempted), 4)
+
+        # Extract remaining variables of interest. 
+        date = row[-1].strip('\n')
+        team = row[-3]
+
+        points = row[11]
+        rebounds = row[3:5]
+        other_stats = row[6:10]
+
+        # Save values
+        clean_row = [team, points, fg_percent, threept_percent, ft_percent] + rebounds + other_stats + [date]
 
         # Write clean data to outfile.
-        filewriter.writerow(clean_row)
+        writer.writerow(clean_row)
