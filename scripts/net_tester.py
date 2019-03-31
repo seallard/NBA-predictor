d56c86945b7f2f42bdae740f8510de79cc3036e2
seallard <seallard95@gmail.com>
2019-03-31 22:40:10 +0200

    model name

--- scripts/net_tester.py
+++ scripts/net_tester.py
@@ -1,8 +1,10 @@
 # Test network (net_1) trained with data from 17-18 season with games from 18-19 season.
+
 import pandas as pd
 import numpy as np
 from keras.models import load_model
 from validation_dataset import team_average
+import math
 
 model = load_model("../trained network/net_1")
 df = pd.read_csv("../data sets/training_dataset.csv")
@@ -12,16 +14,17 @@ made_predictions = 0
 
 for i, game in df.iterrows():
 
-    if i > 510:
-        home_team_averages = team_average(game['home_team'], i, df, "home")
-        away_team_averages = team_average(game['away_team'], i, df, "away")
-        averages = np.asarray([home_team_averages + away_team_averages])
+    home_team_averages = team_average(game['home_team'], i, df, "home", "1")
+    away_team_averages = team_average(game['away_team'], i, df, "away","1")
+    averages = np.asarray([home_team_averages + away_team_averages])
 
-        prediction = int(round(model.predict(averages)[0][0]))
+    prediction = model.predict(averages)[0][0]
+
+    if not math.isnan(prediction):
         made_predictions += 1
         outcome = game['outcome']
-        
-        if prediction == outcome:
+
+        if int(round(prediction)) == outcome:
             correct_predictions += 1
 
 print("Number of predictions made: " + str(made_predictions))
