0fbf90cb556ad2a991e8b3db9140ba1df6314441
seallard <seallard95@gmail.com>
2019-04-09 16:00:43 +0200

    Normalize test data

--- scripts/net_tester.py
+++ scripts/net_tester.py
@@ -7,28 +7,28 @@ import math
 from sklearn.preprocessing import MinMaxScaler
 
 year = "2017_18"
-df = pd.read_csv("../data sets/training_{}.csv".format(year))
-
 scaler = MinMaxScaler()
 
+df = pd.read_csv("../data sets/training_{}.csv".format(year))
+norm_df = scaler.fit_transform(df)
 
-for m in range(20):
+for m in range(10):
 
     model = load_model("../trained network/net_{}".format(m))
 
     correct_predictions = 0
     made_predictions = 0
     
-    for i, game in df.iterrows():
-        test = [game.values[:-1]]
-        normalized_averages = scaler.fit_transform(test)
+    for game in norm_df:
         
-        prediction = model.predict(normalized_averages)[0][0]
+        stats = np.reshape(game[:-1], (-1, len(game[:-1])))
+        prediction = model.predict(stats)[0][0]
+        
 
         if not math.isnan(prediction):
             made_predictions += 1
 
-            if int(round(prediction)) == game['outcome']:
+            if int(round(prediction)) == game[-1]:
                 correct_predictions += 1
 
     print("Model: {}".format(m))
